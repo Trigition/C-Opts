c6dd2d86e92a8bc58e86d370bea9e05ac953b42b
William Fong <trigition@gmail.com>
2017-11-30 13:41:36 -0700

    Implemented free functions and added documentation

--- src/opt.c
+++ src/opt.c
@@ -7,3 +7,80 @@ char *get_help_menu(program_opts *program) {
 char *write_program_opt_header(program_opts *program) {
     return "IMPLEMENT";
 }
+
+// Cleanup functions
+
+/**
+ * @brief This function frees any pointer.
+ * @param occupied_memory A generic pointer.
+ * This function frees any kind of pointer if it actually points
+ * to a valid memory address.
+ */
+void free_pointer(void *occupied_memory) {
+    if (occupied_memory != NULL) free(occupied_memory);
+}
+
+/**
+ * @brief This function frees any \ref opt .
+ * @param option A pointer to an \ref opt
+ * This function completely frees an \ref Any member
+ * of the struct is also freed with it. Any outside reference
+ * must be manually updated to reflect the free.
+ */
+void free_opt(opt *option) {
+    free_pointer(option->flag);
+    free_pointer(option->long_flag);
+    free_pointer(option->help_desc);
+    free_pointer(option->man_desc);
+    free_pointer(option->val);
+    free_pointer(option->value_name);
+
+    free(option);
+}
+
+/**
+ * @brief This function frees any \ref action .
+ * @param program_action A pointer to an \ref action.
+ * This function completely frees an \ref action.
+ * This includes not only each member of an \ref action
+ * struct, but also each subaction. Any outside
+ * references to memory belonging to the subtree will
+ * need to be updated.
+ */
+void free_action(action *program_action) {
+    free_pointer(program_action->action_name);
+    free_pointer(program_action->action_desc);
+    
+    for (unsigned int i = 0; i < program_action->num_flags; i++) {
+        free_opt(program_action->action_opts[i]);
+    }
+
+    // Do a depth first free of the current tree branch
+    for (unsigned int i = 0; i < program_action->num_subactions; i++) {
+        free_action(program_action->subactions[i]);
+    }
+
+    // All reference dependencies have been freed, free the root.
+    free(program_action);
+}
+
+/**
+ * @brief This function frees any \ref program_opts.
+ * @param program A pointer to a \ref program_opts.
+ * This function completely frees a \ref program_opts.
+ * This includes the entire argument tree and each node's
+ * members. Any outside references to any memory
+ * belonging to the tree will need to be updated.
+ */
+void free_program_opt(program_opts *program) {
+    free_pointer(program->program_desc);
+    free_pointer(program->man_desc);
+    free_pointer(program->program_version);
+
+    // Free all program actions
+    for (unsigned int i = 0; i < program->num_actions; i++) {
+        free_action(program->actions[i]);
+    }
+
+    free(program);
+}
--- src/opt.h
+++ src/opt.h
@@ -1,8 +1,11 @@
 #ifndef __OPT_H_
 #define __OPT_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /*! \struct
- *  \brief This struct defines a commandline argument
+ *  @brief This struct defines a commandline argument
  *  This struct encapsulates program arguments. This can range
  *  from positional arguments to positionless flags.
  */
@@ -12,25 +15,27 @@ typedef struct {
     char *help_desc;
     char *man_desc;
     char *val;
+    char *value_name;
     unsigned int opt_pos; // 0th pos considered to be positionless
 } opt;
 
 /*! \struct
- *  \brief This struct defines a program action
+ *  @brief This struct defines a program action
  *  This struct encapsulates program actions. Actions 
  *  dictate different operations a program can perform.
  *  Options are specific to a certain action and its subactions.
  */
-typedef struct {
+typedef struct action {
     char *action_name;
     char *action_desc;
     opt **action_opts;
     struct action **subactions;
-    unsigned int num_children;
+    unsigned int num_flags;
+    unsigned int num_subactions;
 } action;
 
 /*! \struct
- *  \brief This struct defines a program
+ *  @brief This struct defines a program
  *  This struct encapsulates programs. This struct is mainly for
  *  tree root operations for traversal and compilation of the tree
  *  into a C header file.
@@ -40,15 +45,15 @@ typedef struct {
     char *man_desc;
     char *program_version;
     action **actions;
+    unsigned int num_actions;
 } program_opts;
 
 // Begin Function Definitions
 
 // Describe free functions for easy cleanup
 void free_opt(opt *option);
-void free_action(action *action);
+void free_action(action *program_action);
 void free_program_opt(program_opts *program);
-void free_program_tree(program_opts *program);
 
 // 'Constructor' functions for creating arguments
 opt *new_flag(  char *flag_name,
@@ -57,9 +62,16 @@ opt *new_flag(  char *flag_name,
                 char *man_page_description);
 
 opt *new_argument(  char *arg_name,
+                    char *value_name,
                     char *help_description,
                     char *man_page_description,
                     unsigned int position);
+
+opt *valued_flag(   char *flag_name,
+                    char *flag_char,
+                    char *value_name,
+                    char *help_description,
+                    char *man_page_description);
 
 opt *new_action(char *action_name,
                 char *action_description,
