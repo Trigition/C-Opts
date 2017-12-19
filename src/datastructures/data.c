8a7d01af166e899f875e3bf37c0d5ec01ead032f
William Fong <trigition@gmail.com>
2017-12-19 11:09:53 -0700

    Documenting code

--- src/datastructures/data.c
+++ src/datastructures/data.c
@@ -1,5 +1,11 @@
 #include "data.h"
 
+/**
+ * @brief This function creates a new container for data.
+ * @param data The data to be encapsulated.
+ * @param free_func A function specifying how the data should
+ * be freed. Must take a void pointer as an input.
+ */
 container *new_container(void *data, void (*free_func) (void *)) {
     container *n_container = (container *) malloc(sizeof(container));
     n_container->data = data;
@@ -8,6 +14,11 @@ container *new_container(void *data, void (*free_func) (void *)) {
     return n_container;
 }
 
+/**
+ * @brief This function frees a container wrapping data.
+ * @param cur_container The container to be freed. Its data
+ * will be freed along with it.
+ */
 void free_container(container *cur_container) {
     cur_container->references--;
     if (cur_container->references == 0) {
